f2a228a1d2889a6cd9cd44687e34a68c001a5ea7
mlee0214 <mlee29@isl.edu.lv>
2026-03-11 07:49:40 +0000

    1st

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const imageButton = document.getElementById("imageButton");
 const textBox = document.getElementById("textBox");
 
 imageButton.addEventListener("click", function () {
-  scene.style.backgroundImage = 'url("background2.jpg")';
+  scene.style.backgroundImage = 'url("background2.png")';
   imageButton.style.display = "none";
   textBox.style.display = "none";
 });
